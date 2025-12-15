d3348fdbef4d1bc2577a5560272a129b8b8b25f5
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2025-12-16 08:53:21 +0900

    プロジェクト ファイルを追加します。

---
+++ ctl-nginx/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.label_status = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.label_pid = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ファイルToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.操作ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.コマンドプロンプトToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.エクスプローラーで開くToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ウィンドウを閉じるToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.設定ファイルの確認nginxtToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Nginxconfを開くToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PIDから強制終了ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.状態を再取得ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("MS UI Gothic", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 57)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "▶開始"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(233, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "状態："
+        '
+        'label_status
+        '
+        Me.label_status.AutoSize = True
+        Me.label_status.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.label_status.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.label_status.Location = New System.Drawing.Point(286, 38)
+        Me.label_status.Name = "label_status"
+        Me.label_status.Size = New System.Drawing.Size(69, 19)
+        Me.label_status.TabIndex = 2
+        Me.label_status.Text = "起動中"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.Location = New System.Drawing.Point(241, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "PID："
+        '
+        'label_pid
+        '
+        Me.label_pid.AutoSize = True
+        Me.label_pid.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.label_pid.Location = New System.Drawing.Point(286, 74)
+        Me.label_pid.Name = "label_pid"
+        Me.label_pid.Size = New System.Drawing.Size(45, 19)
+        Me.label_pid.TabIndex = 4
+        Me.label_pid.Text = "XXX"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(228, 112)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "ポート："
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ファイルToolStripMenuItem, Me.操作ToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(431, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ファイルToolStripMenuItem
+        '
+        Me.ファイルToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.コマンドプロンプトToolStripMenuItem, Me.エクスプローラーで開くToolStripMenuItem, Me.ウィンドウを閉じるToolStripMenuItem})
+        Me.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem"
+        Me.ファイルToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.ファイルToolStripMenuItem.Text = "ファイル"
+        '
+        '操作ToolStripMenuItem
+        '
+        Me.操作ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.状態を再取得ToolStripMenuItem, Me.設定ファイルの確認nginxtToolStripMenuItem, Me.Nginxconfを開くToolStripMenuItem, Me.PIDから強制終了ToolStripMenuItem})
+        Me.操作ToolStripMenuItem.Name = "操作ToolStripMenuItem"
+        Me.操作ToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.操作ToolStripMenuItem.Text = "操作"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'コマンドプロンプトToolStripMenuItem
+        '
+        Me.コマンドプロンプトToolStripMenuItem.Name = "コマンドプロンプトToolStripMenuItem"
+        Me.コマンドプロンプトToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.コマンドプロンプトToolStripMenuItem.Text = "コマンドプロンプトを開く"
+        '
+        'エクスプローラーで開くToolStripMenuItem
+        '
+        Me.エクスプローラーで開くToolStripMenuItem.Name = "エクスプローラーで開くToolStripMenuItem"
+        Me.エクスプローラーで開くToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.エクスプローラーで開くToolStripMenuItem.Text = "エクスプローラーで開く"
+        '
+        'ウィンドウを閉じるToolStripMenuItem
+        '
+        Me.ウィンドウを閉じるToolStripMenuItem.Name = "ウィンドウを閉じるToolStripMenuItem"
+        Me.ウィンドウを閉じるToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ウィンドウを閉じるToolStripMenuItem.Text = "ウィンドウを閉じる"
+        '
+        '設定ファイルの確認nginxtToolStripMenuItem
+        '
+        Me.設定ファイルの確認nginxtToolStripMenuItem.Name = "設定ファイルの確認nginxtToolStripMenuItem"
+        Me.設定ファイルの確認nginxtToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.設定ファイルの確認nginxtToolStripMenuItem.Text = "シンタックスチェック"
+        '
+        'Nginxconfを開くToolStripMenuItem
+        '
+        Me.Nginxconfを開くToolStripMenuItem.Name = "Nginxconfを開くToolStripMenuItem"
+        Me.Nginxconfを開くToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Nginxconfを開くToolStripMenuItem.Text = "nginx.confを開く"
+        '
+        'PIDから強制終了ToolStripMenuItem
+        '
+        Me.PIDから強制終了ToolStripMenuItem.Name = "PIDから強制終了ToolStripMenuItem"
+        Me.PIDから強制終了ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PIDから強制終了ToolStripMenuItem.Text = "PIDから強制終了"
+        '
+        '状態を再取得ToolStripMenuItem
+        '
+        Me.状態を再取得ToolStripMenuItem.Name = "状態を再取得ToolStripMenuItem"
+        Me.状態を再取得ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.状態を再取得ToolStripMenuItem.Text = "状態を再取得"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(431, 154)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.label_pid)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.label_status)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "NGINX状態確認"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents label_status As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents label_pid As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ファイルToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 操作ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents コマンドプロンプトToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents エクスプローラーで開くToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ウィンドウを閉じるToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 設定ファイルの確認nginxtToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Nginxconfを開くToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PIDから強制終了ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 状態を再取得ToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ ctl-nginx/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Diagnostics
+
+Public Class Form1
+
+    '起動FLAG (True = 起動中 Flase = 停止)
+    Dim nginx_status_flag As Boolean = False
+
+    '実行ファイルのパスを格納する。このEXEとnginx.exeが同じ場所にあることが前提
+    Dim strPath As String = Application.StartupPath '実行中のディレクトリ
+    Dim nginxExe As String = IO.Path.Combine(strPath, "nginx.exe") 'strPathの合わせたnginx.exeのパス
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If nginx_status_flag = True Then
+
+            '起動中なので停止処理を実行する
+            Dim nginx_stop As New ProcessStartInfo() With {
+                .FileName = nginxExe,
+                .Arguments = "-s stop",
+                .WorkingDirectory = strPath,
+                .UseShellExecute = False,
+                .CreateNoWindow = True
+            }
+            Process.Start(nginx_stop)
+
+        Else
+
+            '停止中なので開始処理をする
+            Dim nginx_start As New ProcessStartInfo() With {
+                .FileName = nginxExe,
+                .WorkingDirectory = strPath,
+                .UseShellExecute = False,
+                .CreateNoWindow = True
+            }
+            Process.Start(nginx_start)
+
+        End If
+
+        '開始/停止を確認するために待機する
+        Threading.Thread.Sleep(3000)
+        nginx_check_status()
+
+    End Sub
+
+
+    Private Sub Form_main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If Not IO.File.Exists(nginxExe) Then
+            MessageBox.Show("nginx.exeが見つかりません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Close()
+        End If
+
+        '起動時に状態を確認する
+        nginx_check_status()
+
+    End Sub
+
+    Sub nginx_check_status()
+
+        Dim ps = Process.GetProcessesByName("nginx") ' nginx.exe → "nginx"
+
+        Dim display_pids As String = ""
+
+        If ps.Length = 0 Then
+            ' 動いていない
+            label_status.Text = "Inactive"
+            label_status.BackColor = ColorTranslator.FromOle(RGB(255, 128, 128))
+            label_pid.Text = ""
+            Button1.BackColor = ColorTranslator.FromOle(RGB(128, 255, 128))
+            Button1.Text = "▶START"
+            nginx_status_flag = False
+        Else
+            ' 動いてる（nginxは複数プロセスになることが多い）
+            For Each p In ps
+                Dim pid = p.Id
+                Dim name = p.ProcessName
+                ' 状態表示など
+                display_pids = display_pids & pid & vbCrLf
+            Next
+
+            label_status.Text = "Active"
+            label_status.BackColor = ColorTranslator.FromOle(RGB(128, 255, 128))
+            label_pid.Text = display_pids
+            Button1.BackColor = ColorTranslator.FromOle(RGB(255, 128, 128))
+            Button1.Text = "■STOP"
+
+            nginx_status_flag = True
+
+        End If
+    End Sub
+
+    Private Sub 設定ファイルの確認nginxtToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 設定ファイルの確認nginxtToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub 状態を再取得ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 状態を再取得ToolStripMenuItem.Click
+        nginx_check_status()
+    End Sub
+
+    Private Sub エクスプローラーで開くToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles エクスプローラーで開くToolStripMenuItem.Click
+        Process.Start(strPath)
+    End Sub
+
+    Private Sub コマンドプロンプトToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles コマンドプロンプトToolStripMenuItem.Click
+        Dim cmd_start As New ProcessStartInfo("cmd.exe") With {
+            .Arguments = "/K cd /d """ & strPath & """",
+            .UseShellExecute = True
+        }
+        Process.Start(cmd_start)
+    End Sub
+
+    Private Sub ウィンドウを閉じるToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ウィンドウを閉じるToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub Nginxconfを開くToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Nginxconfを開くToolStripMenuItem.Click
+        Process.Start(strPath & "\conf\nginx.conf")
+    End Sub
+
+    Private Sub PIDから強制終了ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PIDから強制終了ToolStripMenuItem.Click
+
+    End Sub
+End Class
